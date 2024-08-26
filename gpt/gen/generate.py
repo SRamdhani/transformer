a75a340db6f76451c7644cd4c1b6603c0e16e2cc
Satesh Ramdhani <satesh.ramdhani@gmail.com>
2024-08-26 17:15:47 -0400

    Generation complete using psample; initial iteration

---
+++ gpt/gen/generate.py
@@ -0,0 +1,40 @@
+from transformers import AutoTokenizer
+from data.main.data import GPTDATA
+from gpt.model.gpt import GPT
+import torch.nn as nn
+import torch
+import os
+
+
+class GEN:
+
+    @staticmethod
+    def pnucleus(tensor: torch.tensor, p: float = 0.95):
+        values, idx = tensor.sort()
+        psample = idx[values.cumsum(0) > p]
+        return psample[torch.randint(len(psample), (1,))]
+
+    @staticmethod
+    def generate(prompt: str,
+                 length: int,
+                 seq_len: int,
+                 gptmodel: GPT,
+                 gptdata: GPTDATA,
+                 tokenizer: AutoTokenizer):
+
+        tokenized = tokenizer(prompt)['input_ids']
+
+        tokenized.pop(0)
+        tokenized.pop(-1)
+
+        for _ in range(length):
+            excerpt = tokenized[-seq_len:]
+            excerpt_pad, attn = gptdata.padding_and_attn(excerpt, seq_len)
+            excerpt_pad = torch.tensor([excerpt_pad])
+            attn = torch.tensor(attn)
+
+            output = gptmodel(excerpt_pad).squeeze()
+            sampled = GEN.pnucleus(nn.Softmax(dim=0)(output[attn == 1][-1]))
+            tokenized.append(sampled.item())
+
+        return tokenized, tokenizer.decode(tokenized)
--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 from torch.utils.data.dataloader import DataLoader
 from gpt.train.training import TRAIN
 from data.main.data import GPTDATA
+from gpt.gen.generate import GEN
 from gpt.model.gpt import GPT
+from gpt import MODEL_DIR
+import pprint
 import torch
+import os
 
 gptdata = GPTDATA()
 gptdata.curate(max_length=512)
@@ -23,7 +27,29 @@ dataloader = DataLoader(
     batch_size=BATCH_SIZE
 )
 
-TRAIN.run(gptmodel=gptmodel,
-          dataloader=dataloader,
-          batch_size=BATCH_SIZE,
-          epochs=200)
+# TRAIN.run(gptmodel=gptmodel,
+#           dataloader=dataloader,
+#           batch_size=BATCH_SIZE,
+#           epochs=200)
+
+model_path = os.path.join(MODEL_DIR, 'model_weights.pth')
+if os.path.exists(model_path):
+    print('loading pre-existing weights...')
+    gptmodel.load_state_dict(torch.load(model_path))
+
+prompt = """wales want rugby league training wales could follow england s lead by training with a rugby league club.  
+england have already had a three-day session with leeds rhinos  and wales are thought to be interested in a 
+similar clinic with rivals st helens. saints coach ian millward has given his approval  but if it does happen it is
+unlikely to be this season. saints have a week s training in portugal next week  while wales will play england in the 
+opening six nations match on 5 february. we have had an approach"""
+
+_, generated =GEN.generate(prompt,
+                           length=200,
+                           seq_len=SEQ_LEN,
+                           gptmodel=gptmodel,
+                           gptdata=gptdata,
+                           tokenizer=TOKENIZER)
+
+pprint.pp(prompt)
+print()
+pprint.pp(generated)
